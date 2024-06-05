84af58d14c05e670766ebf346026dc1c08333700
Stas Sergeev <stsp@users.sourceforge.net>
2024-06-05 21:30:49 +0300

    add thunk_gen.mk
    This is an include file for makefiles to add thunk_gen rules.

---
+++ scripts/thunk_gen.mk
@@ -0,0 +1,29 @@
+TG = $(shell pkg-config --variable=binary thunk_gen)
+TGS = $(shell pkg-config --variable=tgscript thunk_gen)
+PDS ?= $(shell pkg-config --variable=pdscript thunk_gen)
+TGM4 = $(shell pkg-config --variable=m4script thunk_gen)
+
+GEN_TMP = thunk_calls.tmp thunk_asms.tmp plt.inc plt_asmc.h plt_asmp.h
+_pos = $(if $(findstring $1,$2),$(call _pos,$1,\
+       $(wordlist 2,$(words $2),$2),x $3),$3)
+pos = $(words $(call _pos,$1,$2))
+pars = $(PDS) $(call pos,$@,$(GEN_TMP)) $< >$@ || ($(RM) $@ ; false)
+ifneq ($(PDHDR),)
+$(filter %.tmp,$(GEN_TMP)): $(PDHDR)
+	$(pars)
+plt.inc: thunk_calls.tmp
+	$(pars)
+endif
+plt_asmc.h plt_asmp.h: thunk_asms.tmp
+	$(pars)
+
+thunk_calls.h: thunk_calls.tmp
+	$(TG) $(TFLAGS) <$< >$@
+
+thunk_asms.h: thunk_asms.tmp
+	$(TG) $(TFLAGS) 1 <$< | $(TGS) $(TGM4) >$@_ \
+		|| ($(RM) $@_ ; false)
+	$(TG) $(TFLAGS) 2 <$< >$@__ \
+		|| ($(RM) $@__ ; false)
+	cat $@_ $@__ >$@
+	rm -f $@_ $@__
