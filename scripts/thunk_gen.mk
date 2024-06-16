2c0fb5c39a080858e38f7ae9878c58b185e39d64
Stas Sergeev <stsp@users.sourceforge.net>
2024-06-16 14:27:11 +0300

    thunk_gen.mk: generate plt.inc even w/o PDHDR
    Some environments generate thunk_gen-compatible .tmp files by hands.
    But they should not generate plt.inc by hands.

--- scripts/thunk_gen.mk
+++ scripts/thunk_gen.mk
@@ -8,12 +8,13 @@ _pos = $(if $(findstring $1,$2),$(call _pos,$1,\
        $(wordlist 2,$(words $2),$2),x $3),$3)
 pos = $(words $(call _pos,$1,$2))
 pars = $(PDS) $(call pos,$@,$(GEN_TMP)) $< >$@ || ($(RM) $@ ; false)
+# dj64 generates .tmp files on its own and doesn't set PDHDR
 ifneq ($(PDHDR),)
 $(filter %.tmp,$(GEN_TMP)): $(PDHDR)
 	$(pars)
+endif
 plt.inc: thunk_calls.tmp
 	$(pars)
-endif
 plt_asmc.h plt_asmp.h: thunk_asms.tmp
 	$(pars)
 
